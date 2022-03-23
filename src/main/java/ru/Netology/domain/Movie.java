10f2971937271d02ad2d4744cf2b7263ee741e7c
Demarob <demarulit2@mail.ru>
2022-03-23 22:48:25 +0500

    first commit

---
+++ src/main/java/ru/Netology/domain/Movie.java
@@ -0,0 +1,15 @@
+package ru.Netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Movie {
+    private int id;
+    private int filmId;
+    private String filmName;
+    private String genre;
+}
